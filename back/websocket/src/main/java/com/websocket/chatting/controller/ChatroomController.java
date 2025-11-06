705b360edf4f4dfb0362488734daa69646ead00f
hyogyeong <cdkrcd8@gmail.com>
2025-11-06 16:05:01 +0900

    사용자 정보 및 참여 채팅방 목록 호출

---
+++ back/websocket/src/main/java/com/websocket/chatting/controller/ChatroomController.java
@@ -0,0 +1,29 @@
+package com.websocket.chatting.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.websocket.chatting.dto.Chatroom;
+import com.websocket.chatting.service.ChatroomService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class ChatroomController {
+
+	@Autowired
+	ChatroomService chatroomService;
+	
+	@GetMapping("chatrooms/user/{userId}")
+	public List<Chatroom> getUserChatroomList (@PathVariable int userId){
+		System.out.println("************************"+userId);
+		List<Chatroom> chatrooms = chatroomService.getUserChatrooms(userId);
+		return chatrooms;
+	}
+}
