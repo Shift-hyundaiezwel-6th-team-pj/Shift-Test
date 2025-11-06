0ee38b83f0fda216b58e219cfc673d257935f4eb
hyogyeong <cdkrcd8@gmail.com>
2025-11-06 11:37:30 +0900

    dao 생성

--- back/websocket/src/main/java/com/websocket/chatting/dao/MessageDAO.java
+++ back/websocket/src/main/java/com/websocket/chatting/dao/MessageDAO.java
@@ -3,7 +3,7 @@ package com.websocket.chatting.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.websocket.chatting.model.Message;
+import com.websocket.chatting.dto.Message;
 import com.websocket.chatting.repository.MessageRepository;
 
 @Service
