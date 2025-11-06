bc6010af9a0b244a6c0483443da3bf5c5cf33e23
hyogyeong <cdkrcd8@gmail.com>
2025-11-06 11:33:30 +0900

    dto 생성

---
+++ back/websocket/src/main/java/com/websocket/chatting/dto/Users.java
@@ -0,0 +1,27 @@
+package com.websocket.chatting.dto;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Entity
+@DynamicInsert
+@Table(name="Users")
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "USERID")
+	private String userId;
+	@Column(name = "NAME")
+	private String name;
+}
