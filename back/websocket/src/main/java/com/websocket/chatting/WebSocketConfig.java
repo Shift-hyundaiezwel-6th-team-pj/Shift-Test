518427c18b14c13c8e4e4bf5dd2413950a2f4762
hyogyeong <cdkrcd8@gmail.com>
2025-12-10 22:40:43 +0900

    add fatal data

--- back/websocket/src/main/java/com/websocket/chatting/WebSocketConfig.java
+++ back/websocket/src/main/java/com/websocket/chatting/WebSocketConfig.java
@@ -14,7 +14,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:3000")
+                .setAllowedOriginPatterns(
+                		"http://localhost:3000",
+                		"http://123.123.123.123"
+                		)
                 .withSockJS();
     }
 
